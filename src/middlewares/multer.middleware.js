3bccec3125f03660efad392b1b85cd2203c6aed4
arpit <arpit2003y@gmail.com>
2023-11-12 13:18:29 +0530

    added cloudinary and multer middleware

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer"
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../../public/temp") // keep an eye on it if the package expects something else i need to change it later
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  },
+})
+
+export const upload = multer({
+  storage,
+})
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import fs from "fs"
+import { v2 as cloudinary } from "cloudinary"
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+const uploadOnCloudinary = async (localFIlePath) => {
+  try {
+    if (!localFIlePath) return null
+
+    // uploading process for cloudinary
+    const response = await cloudinary.uploader.upload(localFIlePath, {
+      resource_type: "auto",
+    })
+
+    console.log("Upload successful!", response.url)
+
+    return response
+  } catch (error) {
+    fs.unlink(localFIlePath) // removing the file from the local storage if the upload failed
+    return null
+  }
+}
+
+export { uploadOnCloudinary }

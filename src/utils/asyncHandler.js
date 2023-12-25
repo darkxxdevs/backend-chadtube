94ab19ea4a759d726d0e4a7db596aa91e303eb8f
arpit <arpit2003y@gmail.com>
2023-12-25 13:18:11 +0530

    fix : fixed asynchandler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -3,12 +3,8 @@
 */
 
 const asyncHandler = (requestHandler) => {
-  return async (req, res, next) => {
-    try {
-      await requestHandler(req, res, next)
-    } catch (error) {
-      next(error)
-    }
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
   }
 }
 
